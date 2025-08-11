9ec4951c6c03c141419f7c1d8eeb4d1217b48a59
keitaflora01 <keitafloraprincesses@gmail.com>
2025-08-11 11:05:38 +0100

    fix:conflict

--- gestion/settings.py
+++ gestion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-hk-ub*7og6yp2x$uz0zl+wyl6*f!*+&l8)io1)=e9+n4$wx3^z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
